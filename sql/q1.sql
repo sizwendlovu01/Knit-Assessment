5fac2d1bd05cddd1961e31d75f32f7e5b76e9c94
sizwendlovu01 <sizwe3037@gmail.com>
2026-06-11 14:35:30 +0200

    Sql answers Commit

--- sql/q1.sql
+++ sql/q1.sql
@@ -1,4 +1,4 @@
--- List all active schools in Gauteng ordered by newest first
+-- List of all active schools in Gauteng ordered by newest first
 
 SELECT
     name,
@@ -7,3 +7,5 @@ FROM schools
 WHERE province = 'Gauteng'
   AND is_active = TRUE
 ORDER BY created_at DESC;
+
+--This query returns the name of all the active schools in the province of Gauteng along with their date of creation. I simply added a condition in the WHERE statement for selecting the data for the particular province. Furthermore, sorting is done in descending order according to the value of created_at, which gives the recently created schools at the top. As everything was present in the same table, no JOIN or aggregation statements were required.
--- sql/q2.sql
+++ sql/q2.sql
@@ -12,3 +12,5 @@ JOIN fees f
 WHERE f.status = 'overdue'
 GROUP BY s.id, s.name
 ORDER BY total_overdue_amount DESC;
+
+--In this case, I performed a join between the schools table, the learners table and the fees table so as to find learners who had at least one overdue fee. Filtering based on the fee’s status and then computing the number of learners who had an overdue fee along with the total overdue amount for every school was done. COUNT(DISTINCT l.id) was used since a learner could have several overdue fees and hence there was need to count learners and not the individual fees.
