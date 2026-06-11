e9c144adbb5dae359642f63f6d8ed0641d108a96
sizwendlovu01 <sizwe3037@gmail.com>
2026-06-11 15:09:01 +0200

    Sql answers Commit

--- sql/q3.sql
+++ sql/q3.sql
@@ -11,3 +11,6 @@ LEFT JOIN payments p
     ON p.learner_id = l.id
 WHERE p.id IS NULL
 ORDER BY s.name, l.name;
+
+
+--In this case, I was required to search for those learners who had never made any payments at all. I began by first joining the learners table with the schools table to show the school name along with the name of each of the learners. Then I used a left join between the payments table and the learners table to ensure that all learners appear in the output regardless of whether or not they had made any payments. To get learners who had never made any payments, I specified the payment id field as NULL.
--- sql/q5.sql
+++ sql/q5.sql
@@ -12,3 +12,5 @@ WHERE
     u.last_login IS NULL
     OR u.last_login < CURRENT_DATE - INTERVAL '30 days'
 ORDER BY u.last_login NULLS FIRST;
+
+---For the said search, I had to retrieve inactive users, which meant those who hadn't been logging in within the last 30 days or even hadn't done so ever before. The tables were merged on the users table and schools table since I wanted to display the name of the school. After this, I made an inspection of the last_login value and made sure that I got results for the following two categories: when last_login value is null and when last login date is more than 30 days ago from today’s date.
